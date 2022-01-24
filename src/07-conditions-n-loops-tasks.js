e1bed8fda1be5b166bf8e3c1247b7ca1fe6b62c4
Dina Shchobova <dina.shchobova@gmail.com>
2022-01-24 15:49:57 +0300

    feat: evaluateTicTacToePosition

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -499,8 +499,27 @@ function getMatrixProduct(/* m1, m2 */) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
+function evaluateTicTacToePosition(position) {
+  // throw new Error('Not implemented');
+  for (let i = 0; i < position.length; i += 1) {
+    // eslint-disable-next-line max-len
+    if (position[i][0] !== undefined && position[i][0] === position[i][1] && position[i][1] === position[i][2]) {
+      return position[i][0];
+    }
+    // eslint-disable-next-line max-len
+    if (position[0][i] !== undefined && position[0][i] === position[1][i] && position[0][i] === position[2][i]) {
+      return position[0][i];
+    }
+  }
+  // eslint-disable-next-line max-len
+  if (position[0][0] !== undefined && position[0][0] === position[1][1] && position[0][0] === position[2][2]) {
+    return position[0][0];
+  }
+  // eslint-disable-next-line max-len
+  if (position[0][2] !== undefined && position[0][2] === position[1][1] && position[0][2] === position[2][0]) {
+    return position[0][2];
+  }
+  return undefined;
 }
 
 
